cbe77c9828d0f87da30a20414eb876aa52e740de
3NCRY9T3R <44608474+SKYR4@users.noreply.github.com>
2020-05-07 02:52:57 +0530

    Update game.py

--- Python game/game.py
+++ Python game/game.py
@@ -1,6 +1,8 @@
+import time
+import random
+
+
 def game():
-    import time
-    import random
     c = 0
 
     def choice():
@@ -14,23 +16,27 @@ def game():
             time.sleep(1)
             print("(Please enter 1 or 2.)")
             time.sleep(1)
-            a = int(input())
-            if(a == 1 or a == 2):
+            a = (input())
+            if(a == '1' or a == '2'):
                 return a
                 break
     enemies = ["troll", "wicked fairie", "pirate"]
     enemy = random.choice(enemies)
+
     def start():
-        print("You find yourself standing in an open field, filled with grass and", "yellow wildflowers.")
+        print("You find yourself standing in an open field, filled with grass "
+              "and", "yellow wildflowers.")
         time.sleep(1)
-        print("Rumor has it that a " + enemy + " is somewhere around here, and has", "been terrifying the nearby village.")
+        print("Rumor has it that a " + enemy + " is somewhere around here, and"
+              " has", "been terrifying the nearby village.")
         time.sleep(1)
         print("In front of you is a house.")
         time.sleep(1)
         print("To your right is a dark cave.")
         time.sleep(1)
-        print("In your hand you hold your trusty (but not very effective) dagger.")
+        print("In your hand you hold your trusty (but not effective) dagger.")
         time.sleep(1)
+
     def cave():
         print(" ")
         print("You peer cautiously into the cave.")
@@ -45,89 +51,105 @@ def game():
         time.sleep(1)
         print("You walk back out to the field.")
         time.sleep(1)
+
     def emptycave():
         print(" ")
         print("You peer cautiously into the cave.")
         time.sleep(1)
-        print("You've been here before, and gotten all the good stuff. It's just an empty cave now.")
+        print("You've been here before, and gotten all the good stuff. "
+              "It's just an empty cave now.")
         time.sleep(1)
         print("You walk back out to the field.")
         time.sleep(1)
+
     def door():
         print(" ")
         print("You approach the door of the house.")
         time.sleep(1)
-        print("You are about to knock when the door opens and out steps a " + enemy + " .")
+        print("You are about to knock when the door opens and out steps a "
+              + enemy + " .")
         time.sleep(1)
         print("Eep! This is the " + enemy + " house!")
         time.sleep(1)
         print("The " + enemy + " attacks you!")
         time.sleep(1)
+
     def escape():
         print(" ")
-        print("You run back into the field. Luckily, you don't seem to have been followed.")
+        print("You run back into the field. Luckily, "
+              "you don't seem to have been followed.")
         time.sleep(1)
+
     def victory():
         print(" ")
-        print("As the " + enemy + " moves to attack, you unsheath your new sword.")
+        print("As the " + enemy + " moves to attack, you unsheath your sword.")
         time.sleep(1)
-        print("The Sword of Ogoroth shines brightly in your hand as you brace yourself for the attack.")
+        print("The Sword of Ogoroth shines brightly in your hand as you "
+              "brace yourself for the attack.")
         time.sleep(1)
-        print("But the " + enemy + " takes one look at your shiny new toy and runs away!")
+        print("But the " + enemy + " takes one look at your shiny new toy "
+              "and runs away!")
         time.sleep(1)
-        print("You have rid the town of the " + enemy + " . You are victorious!")
+        print("You have rid the town of the "
+              + enemy + " . You are victorious!")
+
     def fightchoice():
         print(" ")
         print("Would you like to (1) fight or (2) run away?")
-        fch=int(input())
+        fch = (input())
         return fch
+
     def defeat():
         print(" ")
         print("You do your best...")
         time.sleep(1)
-        print("but your dagger is no match for the wicked fairie.")
+        print("but your dagger is no match for the " + enemy)
         time.sleep(1)
         print("You have been defeated!")
         time.sleep(1)
+
     start()
     a = choice()
     while(True):
-        if(a == 1):
+        if(a == '1'):
             door()
             fch = fightchoice()
-            if(fch == 1 and c == 0):
+            if(fch == '1' and c == 0):
                 defeat()
+                exitpoint()
                 break
-            elif(fch == 2):
+            elif(fch == '2'):
                 escape()
                 a = choice()
-            elif(fch == 1 and c != 0):
+            elif(fch == '1' and c != 0):
                 victory()
+                exitpoint()
                 break
-        if(a == 2):
+        if(a == '2'):
             if(c == 0):
                 cave()
                 c = c + 1
-                a=choice()
+                a = choice()
             else:
                 emptycave()
                 a = choice()
+
+
 def exitpoint():
     print("Would you like to play again? (y/n)")
     e = input()
-    return e
-while (True):
-    game()
-    e = exitpoint()
-    while(True):
-        if(e == 'n'):
-            print(" ")
-            print("see you soon!")
-            exit()
-        elif(e == 'y'):
-            print(" ")
-            print("Excellent choice!!, Restarting the game")
-            game()
-            exitpoint()
-        else:
-            exitpoint()
+    if(e == 'n'):
+        print(" ")
+        print("See you soon!")
+        time.sleep(1.5)
+        exit()
+    elif(e == 'y'):
+        print(" ")
+        print("Excellent choice!!, Restarting the game")
+        time.sleep(1.5)
+        game()
+    else:
+        exitpoint()
+
+
+game()
