639702706a16f8537116d8584e87ff6f284ff694
3NCRY9T3R <44608474+SKYR4@users.noreply.github.com>
2020-05-07 10:06:35 +0530

    Update designs.js

--- Pixel Art Maker/designs.js
+++ Pixel Art Maker/designs.js
@@ -1,41 +1,20 @@
-var color = "";
-var sizePicker = document.querySelector("#sizePicker");
-sizePicker.addEventListener("submit", function (event) {
-    event.preventDefault();
-    var heightElement = document.getElementById("inputHeight");
-    var widthElement = document.getElementById("inputWidth");
-    var height = heightElement.value;
-    var width = widthElement.value;
-    grid(height, width);
-})
-function grid(h, w) {
-    var x = 1;
-    var t = "table" + x;
-    var row = document.getElementById(t);
-    while (row !== null) {
-        row.remove();
-        x = ++x;
-        t = "table" + x;
-        row = document.getElementById(t);
-    }
-    var table = document.querySelector("#pixelCanvas");
-    for (var i = 1; i <= h; i++) {
-        table.insertAdjacentHTML("afterbegin", "<tr id = table" + i + "></tr>");
-        var line = document.querySelector("#table" + i);
-        for (var j = 1; j <= w; j++) {
-            line.insertAdjacentHTML("afterbegin","<td id = cell" + i + j +"></td>");
+function changeColor() {
+    const color = document.getElementById("colorPicker").value;
+    this.style.background = color;
+}
+
+function makeGrid() {
+    const height = document.getElementById("input_height").value;
+    const width = document.getElementById("input_width").value;
+    const base = document.getElementById("pixel_canvas");
+    base.innerText=""; //empty default table
+
+    for (let h=0; h<height; ++h) {
+        const row = base.insertRow(-1); //adding new column
+        for (let w=0; w<width; ++w) {
+            const cell = row.insertCell(-1); //adding new row
+            cell.onclick = changeColor; //onclick event handler
         }
     }
-    document.getElementById("pixelCanvas").addEventListener("click", function () {
-        var colorElement = document.getElementById("colorPicker");
-        var color = colorElement.value;
-        var click = event.target.id;
-        var elem = document.getElementById(click);
-        var attrib = elem.hasAttribute("style");
-        if (attrib === true) {
-            elem.removeAttribute("style");
-        } else {
-            elem.style.backgroundColor = color;
-        }
-    })
+    event.preventDefault(); //default
 }
